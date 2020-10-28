d91998047b83bfe16d45c8b29f2114121d1796d5
Meena Ambalam <meena.a.ambalam@gmail.com>
2020-10-28 07:51:59 -0500

    initial directory structure setup as required

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+/*
+
+To run this file, we do the following in our Terminal:
+
+1. Go to the directory of this sql file.
+
+2. Get into our mysql console.
+
+3. Run "source schema.sql"
+
+*/
+
+-- Drops the task_saver_db if it already exists --
+DROP DATABASE IF EXISTS burgers_db;
+
+-- Create the database task_saver_db and specified it for use.
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+-- Create the table tasks.
+CREATE TABLE burgers (
+  id int NOT NULL AUTO_INCREMENT,
+  burger_name varchar(50) NOT NULL,
+  devoured boolean,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beyond Meat Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fish Burger', false);
