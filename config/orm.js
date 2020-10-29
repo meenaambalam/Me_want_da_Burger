11e506d47f7067ea1ab69d386adcec4498d41ad2
Meena Ambalam <meena.a.ambalam@gmail.com>
2020-10-29 01:03:41 -0500

    initial index handlebar design

--- config/orm.js
+++ config/orm.js
@@ -7,11 +7,12 @@ var connection = require("./connection.js");
 // These help avoid SQL injection
 // https://en.wikipedia.org/wiki/SQL_injection
 var orm = {
-  selectAll: function(tableSelect,orderByCol) {
+  selectAll: function(tableSelect,orderByCol,cb) {
     var queryString = "SELECT * FROM ?? ORDER BY ?? ASC";
     connection.query(queryString, [tableSelect,orderByCol], function(err, result) {
       if (err) throw err;
       console.log(result);
+      cb(result);
     });
   },
   insertOne: function(tableInsert,[burgerCol1, burgerCol2],[burgerVal1, burgerVal2],) {
