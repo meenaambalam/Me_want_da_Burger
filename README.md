5d23f26fa1ea369abb4a40ac94a31dcf33b0e323
Meena Ambalam <meena.a.ambalam@gmail.com>
2020-10-29 15:45:54 -0500

    JAWSSQL connection update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Me_want_da_Burger
 App that keeps track of the ordered burgers and the consumed burgers...
+
+Panda Image - https://image.freepik.com/free-vector/cute-panda-eating-burger-cartoon-vector-illustration-animal-food-concept-isolated-vector-flat-cartoon-style_138676-1936.jpg
--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+// Heroku has its own instance of Database which we get with the JAWSDBSQL addon on Heroku for the App....
+// This is what will be used when someone outside the locahost connects to our App
+// It is important to create the table in the Heroku DB instance
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
